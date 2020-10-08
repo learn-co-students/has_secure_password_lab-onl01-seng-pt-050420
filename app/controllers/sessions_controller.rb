95187b9f8e2e8757f892430f79d026318385b752
morgancollado <Morgan.collado@gmail.com>
2020-10-07 19:35:18 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+    def new 
+        @user = User.new
+    end 
+
+    def create 
+        byebug
+        @user = User.find_by(name: params[:name])
+       if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to @user
+       else 
+        flash[:error] = "Login in credentials are incorrect"
+        render :new
+       end 
+    end 
+
+    def destroy 
+        session.delete(:user_id)
+    end 
+
+
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "signup", to: "users#new", as: "signup"
+  get "login", to: "sessions#new", as: "login"
+  post "sessions", to: "sessions#create", as: "sessions"
+
+  delete "sessions", to: "sessions#destroy", as: "logout"
 end
