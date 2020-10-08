cb614ca45ddf5ad4060b2eab1b866586e398eb55
morgancollado <Morgan.collado@gmail.com>
2020-10-07 20:29:06 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,27 @@ class SessionsController < ApplicationController
         @user = User.new
     end 
 
-    def create 
-        @user = User.find_by(name: params[:name])
-       if @user && @user.authenticate(params[:password])
-        session[:user_id] = @user.id
-        byebug
-        redirect_to @user
-       else 
-        flash[:error] = "Login in credentials are incorrect"
-        render :new
-       end 
-    end 
+    # def create 
+    #     @user = User.find_by(name: params[:name])
+    #    if @user && @user.authenticate(params[:user][:password])
+    #     session[:user_id] = @user.id
+    #     byebug
+    #     redirect_to @user
+    #    else 
+    #     flash[:error] = "Login in credentials are incorrect"
+    #     render :new
+    #    end 
+    # end 
+    def create
+        @user = User.find_by(name: params[:user][:name])
+        if @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            redirect_to 'sessions/new'
+        end
+    end
+
 
     def destroy 
         session.delete(:user_id)
