cf4d91e9a26ba912e37074a5ca689e420bcef18e
kanu-e <chukwuemekakanu@gmail.com>
2020-10-20 20:26:29 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'welcome/new'
+
+  get 'sessions/new'
+
+  get 'users/new'
+
+  get '/welcome' => 'welcome#new'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
