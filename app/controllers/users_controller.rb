2c130acc8013597262ed1818c17ba41d2ccc2642
Oscar Ore <60165972+Oscar-Ore007@users.noreply.github.com>
2020-11-01 15:03:47 -0500

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController 
+
+    def create 
+        @user = User.create(user_params)
+        if @user.save && user_params[:password] == user_params[:password_confirmation]
+            @user = User.find_by(name: user_params[:name])
+            session[:user_id] = @user.id 
+
+            redirect_to '/'
+        else 
+            redirect_to '/new'
+    end 
+end 
+
+private 
+
+def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation, :password_digest)
+
+    end 
+end 
